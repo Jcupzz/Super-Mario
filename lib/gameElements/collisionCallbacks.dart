eb4cf00977988ae9c7b10abef42adf25646319d7
Jcupzz <jcupzz255@gmail.com>
2021-09-11 18:34:32 +0530

    jump is corrected

--- lib/gameElements/collisionCallbacks.dart
+++ lib/gameElements/collisionCallbacks.dart
@@ -1,17 +1,46 @@
 import 'package:flame_forge2d/contact_callbacks.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:super_mario_game/SuperMario.dart';
 import 'package:super_mario_game/gameElements/coins.dart';
+import 'package:super_mario_game/gameElements/downBricks.dart';
 import 'package:super_mario_game/gameElements/mario.dart';
+import 'package:super_mario_game/gameElements/pipes.dart';
+import 'package:super_mario_game/gameElements/platform.dart';
 
 class CoinsContactCallback extends ContactCallback<Coins, Mario> {
   @override
-  void begin(Coins coinss, Mario marios, Contact contact) {
+  void begin(Coins coins, Mario mario, Contact contact) {
     print("contacted");
-    coinss.remove();
+    coins.remove();
   }
 
   @override
-  void end(Coins coinss, Mario marios, Contact contact) {
+  void end(Coins coins, Mario mario, Contact contact) {
     print("contacted");
   }
+}
+
+class MarioPlatform extends ContactCallback<Mario, Platform> {
+  SuperMario superMario;
+  @override
+  void begin(Mario mario, Platform platform, Contact contact) {}
+
+  @override
+  void end(Mario mario, Platform platform, Contact contact) {}
+}
+
+class MarioDownBricks extends ContactCallback<Mario, DownBricks> {
+  @override
+  void begin(Mario mario, DownBricks downBricks, Contact contact) {}
+
+  @override
+  void end(Mario mario, DownBricks downBricks, Contact contact) {}
+}
+
+class MarioPipes extends ContactCallback<Mario, Pipes> {
+  @override
+  void begin(Mario mario, Pipes pipes, Contact contact) {}
+
+  @override
+  void end(Mario mario, Pipes pipes, Contact contact) {}
 }
